9fee90b135b45ef259b55f248b42e6d26e61f950
jordyn <jordynmarlow@gmail.com>
2021-04-20 00:39:46 -0400

    finished

--- CanvasHW.js
+++ CanvasHW.js
@@ -1,136 +1,79 @@
 // Jordyn Marlow
 // 4/14/2021
 
-var canvas;
 var context;
 
 // constants
-const TIME_INTERVAL = 25; // screen refresh interval in milliseconds
-const PLANET_TYPES = ["earth.png", "jupiter.png", "mars.png", "mercury.png", "saturn.png", "uranus.png", "venus.png"];
-
-// variables
-var intervalTimer;
-var timeElapsed;
-
-var stars; // array of star objects
-var planets; // array of planet objects
-var starVel; // star speed multiplier
-var planetsVel; // planet speed multiplier
-
-var rocket; // coordinate of rocket
-var rocketAngVel; // rocket angular speed multiplier
+const TIME_INTERVAL = 50; // screen refresh interval in milliseconds
+const PLANET_TYPES = ["earth.png", "jupiter.png", "mars.png", "mercury.png", "saturn.png", "uranus.png", "venus.png", "neptune.png"];
+const PLANET_X = [300, 1200, 700, 1500, 100, 600, 300, 1500];
+const PLANET_Y = [200, 500, 150, 100, 500, 700, 400, 500];
+const PLANET_R = [50, 50, 75, 100, 100, 150, 200, 300];
+const ROCKET_URL = "rocket.png";
 
 var w; // width of the canvas
 var h; // height of the canvas
 
 function init()
 {
-    canvas = document.getElementById("space");
-    context = canvas.getContext("2d");
+    context = document.getElementById("space").getContext("2d");
 
-    w = canvas.width;
-    h = canvas.height;
+    context.canvas.width = 1890;
+    context.canvas.height = 950;
 
-    star = new Array();
-    planets = new Array();
-    rocket = new Object();
-    rocket.img = new Image();
-    rocket.img.src = "rocket.png";
-    rocket.x = 50;
-    rocket.y = 50;
+    w = context.canvas.width;
+    h = context.canvas.height;
 
-    rocket.img.onload = function()
+    generateNewStars(); // randomly draws 50 new stars
+
+    // draw planets
+    for (var i = 0; i < PLANET_TYPES.length; i++)
     {
-        context.drawImage(rocket.img, rocket.x, rocket.y);
+        drawPlanet(PLANET_X[i], PLANET_Y[i], PLANET_R[i], i);
+    }
+
+    generateNewStars(); // randomly draws 50 new stars
+
+    // draw rocket
+    rocket_img = new Image();
+    rocket_img.src = ROCKET_URL;
+    rocket_img.onload = function()
+    {
+        context.drawImage(rocket_img, 900, 300, 150, 450);
     };
-
-    image = new Image();
-    image.src = PLANET_TYPES[type];
-    image.onload = function() { context.drawImage(image, x, y, r, r); };
-
-    //drawPlanet(300, 300, 300, 2);
-    drawStar(10, 10, 10);
-
-    //intervalTimer = window.setInterval(update, TIME_INTERVAL);
 }
 
-/*function update()
+function generateNewStars()
 {
-    var backgroundUpdate = TIME_INTERVAL / 1000.0 * backgroundVel;
-    for (var i = 0; i < background.length; i++)
+    var newStar;
+    for (var i = 0; i < 50; i++)
     {
-        background[i].y += backgroundUpdate;
+        newStar = new Object();
+        newStar.x = Math.floor(Math.random() * w);
+        newStar.y = Math.floor(Math.random() * h);
+        newStar.r = Math.floor(Math.random() * 15);
+        drawStar(newStar.x, newStar.y, newStar.r);
     }
-
-    var foregroundUpdate = TIME_INTERVAL / 1000.0 * foregroundVel;
-    for (var i = 0; i < foreground.length; i++)
-    {
-        foreground[i].y += foregroundUpdate;
-    }
-
-    var planetsUpdate = TIME_INTERVAL / 1000.0 * planetsVel;
-    for (var i = 0; i < planets.length; i++)
-    {
-        planets[i].y += planetsUpdate;
-    }
-
-    var rocketUpdate = TIME_INTERVAL / 1000.0 * rocketAngVel;
-    rocket.ang += rocketUpdate;
-
-    //timeElapsed++;
-
-    draw();
-}
-
-function draw()
-{
-    canvas.width = canvas.width; // clear canvas
-
-    // if any foreground or background objects are out of screen bounds, remove them
-    // draw background, foreground, and planet objects
-    for (var i = 0; i < stars.length; i++)
-    {
-        if (stars[i].y >= h + 50)
-        {
-            stars.splice(i, 1);
-            i--;
-        }
-        drawStar();
-    }
-    for (var i = 0; i < planets.length; i++)
-    {
-        if (planets[i].y >= h + 50)
-        {
-            planets.splice(i, 1);
-            i--;
-        }
-        drawPlanet(planets[i]);
-    }*/
-
-    // possibly add stars and/or planets to arrays for next time step
-    /*var newStar = newObject();
-    newStar.y = 0;
-    stars += newStar;
-
-    // draw the rocket
-
-    drawRocket();
 }
 
 function drawPlanet(x, y, r, type)
 {
-    // draw one of nine random planets
-    image = new Image();
+    var image = new Image();
     image.src = PLANET_TYPES[type];
+    var w = r;
+    if (type == 4) // change width if the planet is Saturn
+    {
+        w = r * (3 / 5);
+    }
     image.onload = function()
     {
-        context.drawImage(image, x, y, r, r);
+        context.drawImage(image, x, y, r, w);
     };
-}*/
+}
 
 function drawStar(x, y, r)
 {
-    var gradient = context.createRadialGradient(x, y, 0, x, y, 10);
+    var gradient = context.createRadialGradient(x, y, 0, x, y, r);
     gradient.addColorStop(0, "white");
     gradient.addColorStop(1, "transparent");
     context.beginPath();
@@ -138,14 +81,5 @@ function drawStar(x, y, r)
     context.fillStyle = gradient;
     context.fill();
 }
-
-/*function drawRocket()
-{
-    // draw rocket rotated to rocket.ang
-    rocket.img.onload = function()
-    {
-        context.drawImage(rocket.img, rocket.x, rocket.y);
-    };
-}*/
 
 window.addEventListener("load", init, false);
